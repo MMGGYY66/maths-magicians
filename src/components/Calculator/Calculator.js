530b6372c57a2e54cf0e7750a0d6d792291c02dd
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-15 09:37:24 -0600

    Add functionality to AC button to clear all numbers and operations on screen

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -8,6 +8,9 @@ import {
 } from './styled';
 
 export default function Calculator() {
+  const [topVal, setTopVal] = useState('');
+  const [operation, setOperation] = useState('');
+
   const [current, setCurrent] = useState('');
 
   const appendValue = (el) => {
@@ -16,28 +19,38 @@ export default function Calculator() {
     setCurrent(current + value);
   };
 
+  const handleAllClear = () => {
+    setCurrent('');
+    setTopVal('');
+    setOperation('');
+  };
+
   return (
     <Container>
       <Screen>
-        <TopValue>12+</TopValue>
+        <TopValue>
+          {topVal}
+          {' '}
+          {operation}
+        </TopValue>
         <BottomValue>{current}</BottomValue>
       </Screen>
-      <Button className="hove">AC</Button>
+      <Button className="hove" onClick={handleAllClear}>AC</Button>
       <Button className="hove">+/-</Button>
-      <Button className="hove">%</Button>
-      <Button className="oranges">÷</Button>
+      <Button className="hove" operation>%</Button>
+      <Button className="oranges" operation>÷</Button>
       <Button data="7" onClick={appendValue} className="hove">7</Button>
       <Button data="8" onClick={appendValue} className="hove">8</Button>
       <Button data="9" onClick={appendValue} className="hove">9</Button>
-      <Button className="oranges">X</Button>
+      <Button className="oranges" operation>X</Button>
       <Button data="4" onClick={appendValue} className="hove">4</Button>
       <Button data="5" onClick={appendValue} className="hove">5</Button>
       <Button data="6" onClick={appendValue} className="hove">6</Button>
-      <Button className="oranges">-</Button>
+      <Button className="oranges" operation>-</Button>
       <Button data="1" onClick={appendValue} className="hove">1</Button>
       <Button data="2" onClick={appendValue} className="hove">2</Button>
       <Button data="3" onClick={appendValue} className="hove">3</Button>
-      <Button className="oranges">+</Button>
+      <Button className="oranges" operation>+</Button>
       <Button data="0" onClick={appendValue} className="zero hove">0</Button>
       <Button data="." onClick={appendValue} className="hove">.</Button>
       <Button className="oranges">=</Button>
