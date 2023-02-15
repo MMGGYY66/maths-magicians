366cb87f5a7ac1472c4859643b4c967b219023ab
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-15 03:46:09 -0600

    Add functionality to the buttons containing numbers to be displayed on the screen

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Screen,
@@ -8,30 +8,38 @@ import {
 } from './styled';
 
 export default function Calculator() {
+  const [current, setCurrent] = useState('');
+
+  const appendValue = (el) => {
+    const value = el.target.getAttribute('data');
+    if (value === '.' && current.includes('.')) return;
+    setCurrent(current + value);
+  };
+
   return (
     <Container>
       <Screen>
         <TopValue>12+</TopValue>
-        <BottomValue>55</BottomValue>
+        <BottomValue>{current}</BottomValue>
       </Screen>
       <Button className="hove">AC</Button>
       <Button className="hove">+/-</Button>
       <Button className="hove">%</Button>
       <Button className="oranges">÷</Button>
-      <Button className="hove">7</Button>
-      <Button className="hove">8</Button>
-      <Button className="hove">9</Button>
+      <Button data="7" onClick={appendValue} className="hove">7</Button>
+      <Button data="8" onClick={appendValue} className="hove">8</Button>
+      <Button data="9" onClick={appendValue} className="hove">9</Button>
       <Button className="oranges">X</Button>
-      <Button className="hove">4</Button>
-      <Button className="hove">5</Button>
-      <Button className="hove">6</Button>
+      <Button data="4" onClick={appendValue} className="hove">4</Button>
+      <Button data="5" onClick={appendValue} className="hove">5</Button>
+      <Button data="6" onClick={appendValue} className="hove">6</Button>
       <Button className="oranges">-</Button>
-      <Button className="hove">1</Button>
-      <Button className="hove">2</Button>
-      <Button className="hove">3</Button>
+      <Button data="1" onClick={appendValue} className="hove">1</Button>
+      <Button data="2" onClick={appendValue} className="hove">2</Button>
+      <Button data="3" onClick={appendValue} className="hove">3</Button>
       <Button className="oranges">+</Button>
-      <Button className="zero hove">0</Button>
-      <Button className="hove">.</Button>
+      <Button data="0" onClick={appendValue} className="zero hove">0</Button>
+      <Button data="." onClick={appendValue} className="hove">.</Button>
       <Button className="oranges">=</Button>
     </Container>
   );
