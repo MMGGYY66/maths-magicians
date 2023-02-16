605da8a66689f47ed4925408b327a2dd565cf58a
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-16 17:39:51 -0600

    Improve functionalities

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,32 +6,18 @@ import {
   BottomValue,
   Button,
 } from './styled';
+import calculate from '../../logic/calculate';
 
 export default function Calculator() {
-  const [topVal, setTopVal] = useState('');
-  const [operation, setOperation] = useState('');
+  const [result, setResult] = useState(0);
+
+  /*
 
   const [current, setCurrent] = useState('');
 
   const appendValue = (el) => {
-    let value = el.target.getAttribute('data');
+    const value = el.target.getAttribute('data');
     if (value === '.' && current.includes('.')) return;
-
-    if (value === '+/-') {
-      let num = parseFloat(current);
-
-      if (current === '' || num <= 0) {
-        value = '+';
-        setOperation(value);
-        setCurrent(String(num));
-      }
-      if (num > 0) {
-        num *= -1;
-        value = '-';
-        setCurrent(String(num));
-        setOperation(value);
-      }
-    }
     setCurrent(current + value);
   };
 
@@ -62,19 +48,9 @@ export default function Calculator() {
       case '+':
         result = topValNumber + currentNumber;
         break;
-      case '+/-':
-        if (topValNumber >= 0 && currentNumber >= 0) {
-          result = -1 * (topValNumber + currentNumber);
-        } else if (topValNumber >= 0 && currentNumber < 0) {
-          result = -1 * (topValNumber + currentNumber);
-        } else if (topValNumber < 0 && currentNumber < 0) {
-          result = -1 * (topValNumber + currentNumber);
-        }
-        break;
       default:
         return;
     }
-    // eslint-disable-next-line consistent-return
     return result;
   };
 
@@ -97,51 +73,55 @@ export default function Calculator() {
     setCurrent('');
     setOperation(el.target.getAttribute('data'));
   };
+*/
+  const handleCLick = (e) => {
+    setResult((prevState) => calculate(prevState, e.target.innerText));
+  };
 
   return (
     <Container>
       <Screen>
-        <TopValue>
-          {topVal}
-          {' '}
-          {operation}
-        </TopValue>
-        <BottomValue>{current}</BottomValue>
+        <TopValue />
+        <BottomValue>{result.next || result.total || 0}</BottomValue>
       </Screen>
-      <Button className="hove" onClick={handleAllClear}>AC</Button>
-      <Button data="+/-" onClick={appendValue} className="hove" operation>+/-</Button>
 
-      <Button data="%" onClick={chooseOperatiion} className="hove" operation>%</Button>
+      <Button className="hove" onClick={handleCLick}>AC</Button>
 
-      <Button data="÷" onClick={chooseOperatiion} className="oranges" operation>÷</Button>
-      <Button data="7" onClick={appendValue} className="hove">7</Button>
-      <Button data="8" onClick={appendValue} className="hove">8</Button>
+      <Button data="+/-" onClick={handleCLick} className="hove" operation>+/-</Button>
 
-      <Button data="9" onClick={appendValue} className="hove">9</Button>
+      <Button data="%" onClick={handleCLick} className="hove" operation>%</Button>
 
-      <Button data="X" onClick={chooseOperatiion} className="oranges" operation>X</Button>
+      <Button data="÷" onClick={handleCLick} className="oranges" operation>÷</Button>
 
-      <Button data="4" onClick={appendValue} className="hove">4</Button>
+      <Button data="7" onClick={handleCLick} className="hove">7</Button>
 
-      <Button data="5" onClick={appendValue} className="hove">5</Button>
+      <Button data="8" onClick={handleCLick} className="hove">8</Button>
 
-      <Button data="6" onClick={appendValue} className="hove">6</Button>
+      <Button data="9" onClick={handleCLick} className="hove">9</Button>
 
-      <Button data="-" onClick={chooseOperatiion} className="oranges" operation>-</Button>
+      <Button data="x" onClick={handleCLick} className="oranges" operation>x</Button>
 
-      <Button data="1" onClick={appendValue} className="hove">1</Button>
+      <Button data="4" onClick={handleCLick} className="hove">4</Button>
 
-      <Button data="2" onClick={appendValue} className="hove">2</Button>
+      <Button data="5" onClick={handleCLick} className="hove">5</Button>
 
-      <Button data="3" onClick={appendValue} className="hove">3</Button>
+      <Button data="6" onClick={handleCLick} className="hove">6</Button>
 
-      <Button data="+" onClick={chooseOperatiion} className="oranges" operation>+</Button>
+      <Button data="-" onClick={handleCLick} className="oranges" operation>-</Button>
 
-      <Button data="0" onClick={appendValue} className="zero hove">0</Button>
+      <Button data="1" onClick={handleCLick} className="hove">1</Button>
 
-      <Button data="." onClick={appendValue} className="hove">.</Button>
+      <Button data="2" onClick={handleCLick} className="hove">2</Button>
 
-      <Button className="oranges" onClick={equals}>=</Button>
+      <Button data="3" onClick={handleCLick} className="hove">3</Button>
+
+      <Button data="+" onClick={handleCLick} className="oranges" operation>+</Button>
+
+      <Button data="0" onClick={handleCLick} className="zero hove">0</Button>
+
+      <Button data="." onClick={handleCLick} className="hove">.</Button>
+
+      <Button className="oranges" onClick={handleCLick}>=</Button>
     </Container>
   );
 }
