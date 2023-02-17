cc45db4c795bb3619e5f33e93ec5b98486b31c47
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-17 08:14:49 -0600

    improve functionalities

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -7,8 +7,6 @@ import {
 } from './styled';
 import calculate from '../../logic/calculate';
 
-import calculate from '../../logic/calculate';
-
 export default function Calculator() {
   const [result, setResult] = useState(0);
 
@@ -21,7 +19,6 @@ export default function Calculator() {
       <Screen>
         <BottomValue>{result.next || result.total || 0}</BottomValue>
       </Screen>
-<<<<<<< HEAD
       <Button className="hove" onClick={handleCLick}>AC</Button>
       <Button data="+/-" onClick={handleCLick} className="hove" operation>+/-</Button>
 
@@ -55,45 +52,6 @@ export default function Calculator() {
 
       <Button data="." onClick={handleCLick} className="hove">.</Button>
 
-=======
-
-      <Button className="hove" onClick={handleCLick}>AC</Button>
-
-      <Button data="+/-" onClick={handleCLick} className="hove" operation>+/-</Button>
-
-      <Button data="%" onClick={handleCLick} className="hove" operation>%</Button>
-
-      <Button data="÷" onClick={handleCLick} className="oranges" operation>÷</Button>
-
-      <Button data="7" onClick={handleCLick} className="hove">7</Button>
-
-      <Button data="8" onClick={handleCLick} className="hove">8</Button>
-
-      <Button data="9" onClick={handleCLick} className="hove">9</Button>
-
-      <Button data="x" onClick={handleCLick} className="oranges" operation>x</Button>
-
-      <Button data="4" onClick={handleCLick} className="hove">4</Button>
-
-      <Button data="5" onClick={handleCLick} className="hove">5</Button>
-
-      <Button data="6" onClick={handleCLick} className="hove">6</Button>
-
-      <Button data="-" onClick={handleCLick} className="oranges" operation>-</Button>
-
-      <Button data="1" onClick={handleCLick} className="hove">1</Button>
-
-      <Button data="2" onClick={handleCLick} className="hove">2</Button>
-
-      <Button data="3" onClick={handleCLick} className="hove">3</Button>
-
-      <Button data="+" onClick={handleCLick} className="oranges" operation>+</Button>
-
-      <Button data="0" onClick={handleCLick} className="zero hove">0</Button>
-
-      <Button data="." onClick={handleCLick} className="hove">.</Button>
-
->>>>>>> e1ff09fa63ac2919e2a901d9f715261877bcbd90
       <Button className="oranges" onClick={handleCLick}>=</Button>
     </Container>
   );
